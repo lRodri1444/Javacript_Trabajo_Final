229158cc64027458505b28cbb6e0cca8e742941c
lRodri144 <luisrodriguezca144@gmail.com>
2022-08-27 12:36:10 -0500

    Ínput validation and alerts

--- script.js
+++ script.js
@@ -3,13 +3,43 @@ const init = function(){
     document.getElementById("new-course-button").addEventListener('click', NewCourseButton);
 }
 
+//Objeto del curso
 let CourseObject = {};
 const thisCourse = Object.create(CourseObject);
 
+//Arrays princiaples
 let all_courses = [];
 let all_grades = [];
 
-//DisplayChartF (Será llamada en Ln 212 y solo se activará una vez)
+//Error de validación para los inputs
+let ValidationErrorMsg = (function(ErrorText, column){
+    let ErrorAlert = document.createElement('div');
+    ErrorAlert.id = 'validation-error';
+    ErrorAlert.className = 'alert alert-danger alert-dismissible fade show';
+    ErrorAlert.setAttribute('role','alert');
+    column.appendChild(ErrorAlert);
+
+    let ErrorMsg = document.createElement('STRONG');
+    ErrorMsg.id = 'validation-error-msg';
+    ErrorMsg.innerText = `${ErrorText}`;
+    document.getElementById('validation-error').appendChild(ErrorMsg);
+
+    let AlertDismissal = document.createElement('button');
+    AlertDismissal.id = 'alert-dismissal';
+    AlertDismissal.type = 'button';
+    AlertDismissal.className = 'close';
+    AlertDismissal.setAttribute('data-dismiss','alert');
+    AlertDismissal.setAttribute('aria-label','Close');
+    document.getElementById('validation-error').appendChild(AlertDismissal);
+
+    let AlertSpan = document.createElement('span');
+    AlertSpan.id = 'validation-error-span';
+    AlertSpan.setAttribute('aria-hidden','true');
+    AlertSpan.innerHTML = "&times;";
+    document.getElementById('alert-dismissal').appendChild(AlertSpan);
+})
+
+//DisplayChartF (Será llamada en Ln 306 y solo se activará una vez)
 var is_displayed = false;
 let DisplayChartF = (function(){
         if (!is_displayed){
@@ -65,6 +95,8 @@ SetAndHide.style = 'margin-left:10px';
 
 //Función tras activar el botón de almacenar datos
 document.getElementById(`set-and-hide-${CourseName}`).onclick = function() {
+    //Token de validación
+    let PercentagesValidator = false;
 
     //Guardando los porcentajes de los input en un array
     let percentage_input = document.getElementsByClassName(`percentages-to-set-${CourseName}`);
@@ -72,8 +104,48 @@ document.getElementById(`set-and-hide-${CourseName}`).onclick = function() {
     for (let i of percentage_input) {
         percentages_array.push(parseInt(i.value));
     }
+
+    //Array reducido
+    let percentages_sum = percentages_array.reduce(function(acc, cur) {
+        return acc + cur;
+    },0);
+
+    //Validando
+    for (let i of percentages_array){
+        if (i < 0 || i > 100){
+            const PercentagesErrorText = 'Algo salió mal, recuerda:\n• Solo puedes ingresar porcentajes entre 0 y 100 \n• La suma de tus porcentajes debe ser 100';
+            const FirstColumn = document.getElementById('first-column');
     
+            PercentagesValidator = false;
+            percentages_array = [];            
+            if (!document.getElementById('validation-error')){
+            ValidationErrorMsg(PercentagesErrorText, FirstColumn);
+            }
+            break;  
+        } else{
+            if (document.getElementById('validation-error')){
+                document.getElementById('validation-error').remove();
+            }
+            PercentagesValidator = true;
+        }
+    }
+
+    if (percentages_sum != 100){
+        const PercentagesSumErrorText = `Algo salió mal, recuerda:\n• Solo puedes ingresar porcentajes entre 0 y 100 \n• La suma de tus porcentajes debe ser 100`;
+        const FirstColumn = document.getElementById('first-column');
+
+        PercentagesValidator = false;
+        percentages_array = [];
+        if (!document.getElementById('validation-error')){
+            ValidationErrorMsg(PercentagesSumErrorText, FirstColumn);
+        }
+        percentages_sum = 0;
+    } else{
+        PercentagesValidator = true;
+    }
+
     //Desapareciendo los input y el botón tras guardar data
+    if (PercentagesValidator === true){
     const percentages = document.getElementsByClassName(`percentages-to-set-${CourseName}`);
     for (let i of percentages) {
         i.style.display = 'none';
@@ -147,24 +219,46 @@ document.getElementById(`set-and-hide-${CourseName}`).onclick = function() {
         }
         )
 
-        //Botón para enviar data
-        const SendAndShow = document.createElement('button');
-        SendAndShow.textContent = 'Calcular';
-        SendAndShow.id = 'send-and-show-button';
-        SendAndShow.className = 'btn btn-dark';
-        document.getElementById('second-column').appendChild(SendAndShow);
+            //Botón para enviar data
+            const SendAndShow = document.createElement('button');
+            SendAndShow.textContent = 'Calcular';
+            SendAndShow.id = 'send-and-show-button';
+            SendAndShow.className = 'btn btn-dark';
+            document.getElementById('second-column').appendChild(SendAndShow);
 
-            
-        //Iniciando el evento del botón
-        document.getElementById('send-and-show-button').onclick = function() {
+            //Iniciando el evento para calcular notas
+            document.getElementById('send-and-show-button').onclick = function() {
+            //Token de validación
+            let GradesValidator = false;
 
-            //Looping a través de los input y guardándo los valores en un array
+            //Guardándo las notas en un array
             const entered_grades = document.getElementsByClassName('grade-input');
             for (let i of entered_grades){
                 const the_grade = i.value;
                 each_exam_grade.push(the_grade);
             }
 
+            //Validando las notas
+            for (let i of each_exam_grade){
+                if (i < 0 || i > 10){
+                    const GradesErrorText = 'Solo puedes ingresar notas entre 0 y 10';
+                    const ThirdColumn = document.getElementById('third-column');
+
+                    GradesValidator = false;
+                    each_exam_grade = [];
+                    if (!document.getElementById('validation-error')){
+                        ValidationErrorMsg(GradesErrorText, ThirdColumn);
+                    }
+                    break;
+                } else{
+                    if (document.getElementById('validation-error')){
+                        document.getElementById('validation-error').remove();
+                    }
+                    GradesValidator = true;
+                }
+            }
+
+            if (GradesValidator === true){       
             //Removemos el <ul> que contiene los <li> e <input> y volvemos a mostrar el primer <ul>
             document.getElementById(`${CourseName}-input-ul`).remove();
             document.getElementById('send-and-show-button').remove();
@@ -173,59 +267,59 @@ document.getElementById(`set-and-hide-${CourseName}`).onclick = function() {
                 btn_to_hide.style.display = 'block';
             }
 
-        //Actualizamos objeto con las notas de cada evaluación 
-        CourseObject.each_grade = each_exam_grade;
-        const {each_grade:theCourseEachGrade} = CourseObject;
+            //Actualizamos objeto con las notas de cada evaluación 
+            CourseObject.each_grade = each_exam_grade;
+            const {each_grade:theCourseEachGrade} = CourseObject;
 
-        //Segundo bucle : asignar una nota a cada porcentaje y calcular
-        let recalculated_grades_array = [];
+            //Segundo bucle : asignar una nota a cada porcentaje y calcular
+            let recalculated_grades_array = [];
 
-        theCourseEachEvaluation.forEach((percentage, grade) => {
-            const grade_to_calculate = theCourseEachGrade[grade];
-            const recalculated_grades = ((percentage/100)*grade_to_calculate);
-            recalculated_grades_array.push(recalculated_grades)
-        }
-        )
+            theCourseEachEvaluation.forEach((percentage, grade) => {
+                const grade_to_calculate = theCourseEachGrade[grade];
+                const recalculated_grades = ((percentage/100)*grade_to_calculate);
+                recalculated_grades_array.push(recalculated_grades)
+            }
+            )
 
-        //Reduciendo el array recalculado en una nota final del curso
-        const final_course_grade = recalculated_grades_array.reduce(function(acc, cur) {
-            return acc + cur;
-        },0
-        )
+            //Reduciendo el array recalculado en una nota final del curso
+            const final_course_grade = recalculated_grades_array.reduce(function(acc, cur) {
+                return acc + cur;
+            },0
+            )
 
-        //Actualizando el objeto con la nota final
-        CourseObject.final_grade = final_course_grade;
-        const {final_grade:theCourseFinalGrade} = CourseObject;
+            //Actualizando el objeto con la nota final
+            CourseObject.final_grade = final_course_grade;
+            const {final_grade:theCourseFinalGrade} = CourseObject;
 
-        //Desactivando la calculadora ya usada e insertando la nota como un párrafo (De momento)
-        document.getElementById(`course-list-for-${CourseName}`).style = NaN
+            //Desactivando la calculadora ya usada e insertando la nota como un párrafo (De momento)
+            document.getElementById(`course-list-for-${CourseName}`).style = NaN
 
-        document.getElementById(`${CourseName}-calculator`.toLocaleLowerCase()).remove();
-        let disabled_p = (document.createElement('p')).appendChild(document.createTextNode(`${CourseName}`));
-        (document.getElementById(`course-list-for-${CourseName}`)).appendChild(disabled_p);
+            document.getElementById(`${CourseName}-calculator`.toLocaleLowerCase()).remove();
+            let disabled_p = (document.createElement('p')).appendChild(document.createTextNode(`${CourseName}`));
+            (document.getElementById(`course-list-for-${CourseName}`)).appendChild(disabled_p);
 
-        //Pusheando los resultados
-        all_courses.push(theCourseName);
-        all_grades.push(theCourseFinalGrade);
+            //Pusheando los resultados
+            all_courses.push(theCourseName);
+            all_grades.push(theCourseFinalGrade);
 
-        //Llamando a DisplayChartF (Ln 14)
-        DisplayChartF();
+            //Llamando a DisplayChartF (Ln 44)
+            DisplayChartF();
 
-        //Almacenamiento local
-        localStorage.setItem('courses-serialized', '[]');
-        let courses_deserialized = JSON.parse(localStorage.getItem('courses-serialized'));
-        courses_deserialized.push(...all_courses);
-        localStorage.setItem('courses-serialized', JSON.stringify(courses_deserialized));
+            //Almacenamiento local
+            localStorage.setItem('courses-serialized', '[]');
+            let courses_deserialized = JSON.parse(localStorage.getItem('courses-serialized'));
+            courses_deserialized.push(...all_courses);
+            localStorage.setItem('courses-serialized', JSON.stringify(courses_deserialized));
 
-        localStorage.setItem('grades-serialized', '[]');
-        let grades_deserialized = JSON.parse(localStorage.getItem('grades-serialized'));
-        grades_deserialized.push(...all_grades);
-        localStorage.setItem('grades-serialized', JSON.stringify(grades_deserialized));
+            localStorage.setItem('grades-serialized', '[]');
+            let grades_deserialized = JSON.parse(localStorage.getItem('grades-serialized'));
+            grades_deserialized.push(...all_grades);
+            localStorage.setItem('grades-serialized', JSON.stringify(grades_deserialized));
 
-        //Activando los input nuevamente
-        document.getElementById("new-course-button").disabled = false;
-        document.getElementById("new-course-name").disabled = false;
-        document.getElementById('number-of-exams').disabled = false; 
+            //Activando los input nuevamente
+            document.getElementById("new-course-button").disabled = false;
+            document.getElementById("new-course-name").disabled = false;
+            document.getElementById('number-of-exams').disabled = false; 
 
         //Ejecución de Charts JS 
         document.getElementById('display-chart').onclick = function() {
@@ -309,9 +403,11 @@ document.getElementById(`set-and-hide-${CourseName}`).onclick = function() {
                   document.getElementById('results-to-download').appendChild(base64_img);
                 }
               })();
+        }
         }   
         }       
-    }   
+    }
+}   
 }
 }
 //Carga el DOM
